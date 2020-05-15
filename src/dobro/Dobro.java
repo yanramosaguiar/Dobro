43c6da405b018d18f4b31254a22fbfba8ad331fb
yanramosaguiar <aguiar.yan1@gmail.com>
2020-05-14 22:39:40 -0300

    Dobro

---
+++ src/dobro/Dobro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dobro;
+
+/**
+ *
+ * @author Yan
+ */
+public class Dobro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Tela().setVisible(true);
+    }
+    
+}
